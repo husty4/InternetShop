b7661b0396d1d8a00af4b58ef445720580a73f3a
husty4 <olehhryhlevych@gmail.com>
2023-12-05 20:05:29 +0200

    Initial commit

---
+++ src/test/java/com/olehhryhlevych/InternetShop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olehhryhlevych.InternetShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
